5024351232ce890677a59085f98abef01e5c19e1
earthbound19 <ansible1@earthbound.io>
2017-08-19 12:39:21 -0600

    Fixed inconsistent indent characters and broke ;s into lines for legibility

--- gibGenPython/gibGen.py
+++ gibGenPython/gibGen.py
@@ -57,10 +57,11 @@ for i in range(0, genNumPhonemes):
     # It may turn out that using for loops is the most legible and fast way to do this, re https://stackoverflow.com/a/1156143/1397555 :
         # LOOK FOR the whole set of possible matches for mustStartWith and add them to a list of lists firstCharMatchedList; summing the occurances given from the source .mkvch into frequencySum to be used later.
     for pair, idx in data:
-    	partA = pair[0]        # pair[0] is e.g. 'a', the first character in pair; (pair[0][0] + pair[0][1]) would be both e.g. 'ab'.
-    	if partA == mustStartWith:
-            # FREQUENCYSUM totals number part of pair through loop of (data) pairs starting with a given letter.
-    		frequencySum = (frequencySum + int(idx)); tmpTuple = [pair, frequencySum]; firstCharMatchedList.append(tmpTuple); logFile.write('appended partA value \'' + partA + '\' from pair \'' + pair + '\' with frequency ' + idx + '\nfrequencySum: ' + str(frequencySum) + '\n')
+        partA = pair[0]        # pair[0] is e.g. 'a', the first character in pair; (pair[0][0] + pair[0][1]) would be both e.g. 'ab'.
+        if partA == mustStartWith:      # Previously, some other unknown indent characters for the next lines, Python hated (and tried to tell me, but the error messages mystified me), as everything else here was something else (tabs? is?).
+            frequencySum = (frequencySum + int(idx))
+            tmpTuple = [pair, frequencySum]
+            firstCharMatchedList.append(tmpTuple); logFile.write('appended partA value \'' + partA + '\' from pair \'' + pair + '\' with frequency ' + idx + '\nfrequencySum: ' + str(frequencySum) + '\n')
     # WITH FIRSTCHARMATCHEDLIST populated with matches copied from var data[lists], randomly pick one of the pairs by first character in pair matching mustStartWith, accounting for statistical frequency (akin to data[0][1] == 4426). If there will not be a match (akin to data[0][0][0] == 'a') for mustStartWith, pick any first letter from any pair in the whole data list of lists.
 
         # IF A MATCH WAS FOUND (frequencySum != 0), assign it to nextLetter for later recombobulation.
@@ -70,11 +71,18 @@ for i in range(0, genNumPhonemes):
         for idx, pair in enumerate(firstCharMatchedList):
             # idx == iteration in loop, pair e.g. == [' a', 630737], pair[0] == ' a', pair[1] == 630737. TO DO: learn, would a third var listing in the for enumeration get the second list item in the list at firstCharMatchedList(idx)?
             if pair[1] >= PRND:
-                pickedPair = str( firstCharMatchedList[ (idx - 1) ] ); nextLetter = pickedPair[3]; logFile.write('! ----------- PICK \'' + nextLetter + '\' for pair ' + pickedPair + ' at num ' + str(pair[1]) + '!\n'); break
+                pickedPair = str( firstCharMatchedList[ (idx - 1) ] )
+                nextLetter = pickedPair[3]
+                logFile.write('! ----------- PICK \'' + nextLetter + '\' for pair ' + pickedPair + ' at num ' + str(pair[1]) + '!\n')
+                break
         # IF A MATCH WAS NOT FOUND (frequencySum == 0), terminate the word by setting nextLetter to ' ', and set mustStartWith to a random selection from the entire data set of first letters (that appear in a group).
     else:
         print('YO!')
-        nextLetter = ' '; dataLen = len(data); PRND = randint(0, (dataLen - 1) ); logFile.write('No match starts with \'' + mustStartWith + '\'; terminated word and picking mustStartWith val from data set . . .\n'); mustStartWith = data[ (PRND) ][0][0]; logFile.write('PICKED new value \'' + mustStartWith + '\' for mustStartWith.\n')
+        nextLetter = ' '
+        dataLen = len(data)
+        PRND = randint(0, (dataLen - 1) )
+        logFile.write('No match starts with \'' + mustStartWith + '\'; terminated word and picking mustStartWith val from data set . . .\n'); mustStartWith = data[ (PRND) ][0][0]
+        logFile.write('PICKED new value \'' + mustStartWith + '\' for mustStartWith.\n')
 
     # WHATEVER HAPPENED for the value of frequencySum, we covered all possible cases to assign to nextLetter, so can do this now:
     recombobulation = (recombobulation + nextLetter)
