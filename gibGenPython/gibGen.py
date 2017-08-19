b540db34ed6a1382fa3b73c7b5796930eea1bef0
earthbound19 <ansible1@earthbound.io>
2017-08-19 11:37:12 -0600

    comment concluding word length statistics or summat necessary

--- gibGenPython/gibGen.py
+++ gibGenPython/gibGen.py
@@ -9,8 +9,7 @@
 # NOTES: This may only work with python3. Also, the only source database I have producing useful output at this writing is databases/TheJabberwocky.mkvch, at this writing. TO DO: fix that: maybe by having this work statistically on trigrams instead of bigrams.
 
 # TO DO
-# if no match found in loop in prior letter match iteration, terminate word (print a space), and randomly pick a new letter to start another word. AND/OR (potentially two different problems, and maybe only this second one is a valid problem), whatever condition leads to the problem in the log noted in this writing in charactersThatBreakIAEgibGen.txt -- fix that.
-# make it write to files (instead of stdout), as stdout will fail on some unicode chars.
+# Well, shucks. It seems that bigram statistics alone including spaces isn't going to end words very often (like, extremely not often). Force statistical inflation of space character ' ' (for word endings) AND/OR compile word length stats and employ them for artificial cutoff. Maybe this wouldn't be a problem with trigrams. With bigrams it's clearly a problem.
 # make log file optional (which means make a log function and refactor to pass it strings conditionally)
 # dump recombobulation var to file when it gets to certain huge size, then continue filling it.
 # make gibberish dump file name partly based on timestamp.
@@ -24,57 +23,65 @@ import argparse     # to parse terminal arguments
 
 # Set up and make use of command line arguments; use defaults if none passed.
 argParser = argparse.ArgumentParser()
-argParser.add_argument("-s", "--source", help="source .mkvch database filename")
+argParser.add_argument("-d", "--database", help="database (source).mkvch database filename")
 argParser.add_argument("-c", "--count", help="how many letter groups to create")
 args = argParser.parse_args()
-if args.source:
-    print('Source db ' + args.source + ' specified.')
+if args.database:
+    print('Source db ' + args.database + ' specified.')
 else:
-    args.source = 'databases/TheJabberwocky.mkvch'
+    args.database = 'databases/IAE.mkvch'
 if args.count:
     print('Letter groups to create number ' + args.count + ' specified.')
 else:
     args.count = 1000
 
-with codecs.open(args.source, 'r', encoding='utf-8') as inputFile:
+# READ database into var data
+with codecs.open(args.database, 'r', encoding='utf-8') as inputFile:
     reader = csv.reader(inputFile)
-    data = list(reader)		# I've verified that will write back to a file the exact same structure of a source database, if parsed correctly.
+    data = list(reader)		# e.g. data[0] == ['aa', '4426'], data[0][0] is: aa, data[0][1] is: 4426. I've verified that this object list data[lists] will write back to a file the exact same structure of a source database, if parsed correctly.
 inputFile.close
 
 # open gibberish generation output file for writing; TO DO: generate this file name based on source database and date?
 logFile = codecs.open('gibGen_py_log.txt', 'w', encoding='utf-8')
+
 
 recombobulation = ''
 # seed mustStartWith var with space ' '
 mustStartWith = ' '
 genNumPhonemes = int(args.count)
 frequencySum = 0
-matchedPairs = list()      # This will be a list of lists!
+firstCharMatchedList = list()      # This will be a list of lists!
 # loop as many times as genNumPhonemes var:
 for i in range(0, genNumPhonemes):
-    logFile.write('---------- GENERATING phoneme------------\n')
-    # find pair that starts with mustStartWith; and it may turn out that using for loops is the most legible and fast way to do this, re https://stackoverflow.com/a/1156143/1397555 :
-    			# e.g. data[0] is "printed" by python as: ('a', 'g')
-    			# -- and data[0][0] is: 'a' and data[0][1] is: 'g'
-    for pair, idx in data:            # e.g. a list within the data list, ('a', 'g')
-    	partA = pair[0]               # the first character in pair; pair[0][0] + pair[0][1] would be both.
+    logFile.write('---------- FINDING next phoneme ---------- \n')
+    # It may turn out that using for loops is the most legible and fast way to do this, re https://stackoverflow.com/a/1156143/1397555 :
+        # LOOK FOR the whole set of possible matches for mustStartWith and add them to a list of lists firstCharMatchedList; summing the occurances given from the source .mkvch into frequencySum to be used later.
+    for pair, idx in data:
+    	partA = pair[0]        # pair[0] is e.g. 'a', the first character in pair; (pair[0][0] + pair[0][1]) would be both e.g. 'ab'.
     	if partA == mustStartWith:
-    		frequencySum = (frequencySum + int(idx)); tmpTuple = [pair, frequencySum]; matchedPairs.append(tmpTuple); logFile.write('appended partA value \'' + partA + '\' from pair \'' + pair + '\' with frequency ' + idx + '\nfrequencySum: ' + str(frequencySum) + '\n')
-    # With matchedPairs populated from iterated items pair, randomly pick one of the pairs accounting for statistical frequency of pair occurances indexed from .mkvch database. 
-    PRND = randint(0, frequencySum)
-    # Reset frequencySum for the following for loop:
-    frequencySum = 0
-    logFile.write('Selected num ' + str(PRND) + ' from range 0,' + str(frequencySum) + '\n')
-    for idx, pair in enumerate(matchedPairs):       # TO DO: learn, would a third var listing there get the second list item in the list at matchedPairs(idx)?
-        # FIX to go here? :
-        if PRND == 0:
-            logFile.write('ERROR: Oh teh noes! It picked zero! Something wrong . . .\n wheh matchedPairs len ' + str(pickedPair))
-        if pair[1] >= PRND:
-            pickedPair = str( matchedPairs[ (idx - 1) ] ); nextLetter = pickedPair[3]; logFile.write('! ----------- PICK for pair ' + pickedPair + ' at num ' + str(pair[1]) + '!\n'); recombobulation = (recombobulation + nextLetter); mustStartWith = nextLetter; break
-            # ; print(pickedPair + ' \'' + nextLetter + '\'')
+            # FREQUENCYSUM totals number part of pair through loop of (data) pairs starting with a given letter.
+    		frequencySum = (frequencySum + int(idx)); tmpTuple = [pair, frequencySum]; firstCharMatchedList.append(tmpTuple); logFile.write('appended partA value \'' + partA + '\' from pair \'' + pair + '\' with frequency ' + idx + '\nfrequencySum: ' + str(frequencySum) + '\n')
+    # WITH FIRSTCHARMATCHEDLIST populated with matches copied from var data[lists], randomly pick one of the pairs by first character in pair matching mustStartWith, accounting for statistical frequency (akin to data[0][1] == 4426). If there will not be a match (akin to data[0][0][0] == 'a') for mustStartWith, pick any first letter from any pair in the whole data list of lists.
+
+        # IF A MATCH WAS FOUND (frequencySum != 0), assign it to nextLetter for later recombobulation.
+    if frequencySum != 0:
+        PRND = randint(0, frequencySum)
+        logFile.write('Selected PRND ' + str(PRND) + ' from range 0,' + str(frequencySum) + '\n')
+        for idx, pair in enumerate(firstCharMatchedList):
+            # idx == iteration in loop, pair e.g. == [' a', 630737], pair[0] == ' a', pair[1] == 630737. TO DO: learn, would a third var listing in the for enumeration get the second list item in the list at firstCharMatchedList(idx)?
+            if pair[1] >= PRND:
+                pickedPair = str( firstCharMatchedList[ (idx - 1) ] ); nextLetter = pickedPair[3]; logFile.write('! ----------- PICK \'' + nextLetter + '\' for pair ' + pickedPair + ' at num ' + str(pair[1]) + '!\n'); break
+        # IF A MATCH WAS NOT FOUND (frequencySum == 0), terminate the word by setting nextLetter to ' ', and set mustStartWith to a random selection from the entire data set of first letters (that appear in a group).
+    else:
+        print('YO!')
+        nextLetter = ' '; dataLen = len(data); PRND = randint(0, (dataLen - 1) ); logFile.write('No match starts with \'' + mustStartWith + '\'; terminated word and picking mustStartWith val from data set . . .\n'); mustStartWith = data[ (PRND) ][0][0]; logFile.write('PICKED new value \'' + mustStartWith + '\' for mustStartWith.\n')
+
+    # WHATEVER HAPPENED for the value of frequencySum, we covered all possible cases to assign to nextLetter, so can do this now:
+    recombobulation = (recombobulation + nextLetter)
     # reset frequencySum for the next iteration of the outmost loop:
     frequencySum = 0
 # for loop ends
+
 
 logFile.close
 
