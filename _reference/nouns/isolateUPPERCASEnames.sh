e4a8fc7af0ab0f044122de3a30302ed364c81a41
earthbound19 <ansible1@earthbound.io>
2017-08-23 12:24:11 -0600

    cultivated evolutionary policies

--- _reference/nouns/isolateUPPERCASEnames.sh
+++ _reference/nouns/isolateUPPERCASEnames.sh
@@ -8,4 +8,7 @@
 # Pass one parameter to this script, being the text file to filter, e.g.:
 # thisScript.sh textFileToFilter.txt
 
-gsed -f isolateUPPERCASEname.sed < $1
+gsed -f isolateUPPERCASEnames.sed < $1 > oot1.txt
+sort oot1.txt > oot2.txt
+uniq oot2.txt > isolated.txt
+rm oot1.txt oot2.txt
