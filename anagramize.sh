2de5a063ff7595834807042c37ed376bb23e7265
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-18 00:18:32 -0600

    - error out if no $1 - improve doc. layout per new pref. - gsed -> sed - print progress feedback - source and target files are named parameters

--- anagramize.sh
+++ anagramize.sh
@@ -1,23 +1,36 @@
-# DESCRIPTION: Takes an input text paramater and outputs an ~_anagramized.txt text file where every vowel may be scrambled with an adjacent character. The result is readable to (about half of?) humans, but would take extra programming or resources for web keyword scanners (SPIES) to make good of.
+# DESCRIPTION
+# Takes an input text paramater and outputs an ~_anagramized.txt text file where every vowel may be scrambled with an adjacent character. The result is readable to (about half of?) humans, but would take extra programming or resources for web keyword scanners (SPIES) to make good of.
 
-# USAGE: run the script with one paramaeter, being the name of a text file in the same directory, to scramble. Results will appear in ~_anagramized.txt. ex.:
-# ./thisScript.sh inputFile.txt
+# USAGE
+# Run with one paramaeter:
+# The name of a text file to scramble, including the path to it if it is not in the current directory. Results will appear in ~_anagramized.txt.
 
-# OPTIONAL: Strip every character that isn't alphabetic or a space:
-# gsed "s/[^[:alpha:] ]/ /g" $1 > tmp1.txt
-# IF YOU DON'T DO THAT previous line, do this:
-cat $1 > tmp1.txt
-tr ' ' '\n' < tmp1.txt > tmp2.txt
-printf "" > tmp3.txt
+# CODE
+if [ ! "$1" ]; then printf "\nNo parameter \$1 (source file to anagramize) passed to script. Exit."; exit 1; else sourceFile=$1; fi
+targetFile=${sourceFile%.*}__anagramized.txt
 
-while read element
+echo "Anagramizing $sourceFile . . ."
+
+# kludge to keep punctuation clearer if oddly spaced bcse sed command my not make skip punctuation:
+sed 's/\(\.,;:!?\"\)/ \1 /g' $sourceFile > temp1.txt
+
+tr ' ' '\n' < temp1.txt > temp2.txt
+mapfile -t words < temp2.txt
+rm temp1.txt temp2.txt
+
+# Create blank target file before writing to it:
+printf "" > $targetFile
+
+for element in "${words[@]}"
 do
-	# adapted from: http://stackoverflow.com/a/26326317 and http://www.theunixschool.com/2012/06/swap-every-2-lines-in-file.html
-	echo $element | gsed 's/[aeiouyAEIOUY]/&\n/g' | gsed -n '{h;${p;q;};n;G;p;}' | tr -d '\n' > tmp1.txt
-	scrambledWord=$( < tmp1.txt )
-	echo "$scrambledWord " >> tmp3.txt
-done < tmp2.txt
+	# adapted from: http://stackoverflow.com/a/26326317
+	echo $element | sed 's/[aeiouyAEIOUY]/&\n/g' | shuf | tr -d "\n" > temp2.txt
+	scrambledWord=$( < temp2.txt )
+	echo $scrambledWord >> $targetFile
+done
 
-tr '\n' ' ' < tmp3.txt > $1\_anagramized.txt
-gsed -i 's/  / /g' $1\_anagramized.txt
-rm tmp1.txt tmp2.txt tmp3.txt
+tr '\n' ' ' < $targetFile > temp1.txt
+rm $targetFile temp2.txt
+mv temp1.txt $targetFile
+
+echo "Done. Result is in file $targetFile."
